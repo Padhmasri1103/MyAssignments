112d3b841f6f003a9f013ce31db65e858b34522e
Padhmasri1103 <srigopu11@gmail.com>
2025-10-15 13:40:43 +0530

    Add files via upload
    Day1 - JavaScript Datatypes

---
+++ task1.js
@@ -0,0 +1,15 @@
+
+var firstName = 'Pavi'; 
+console.log("first name is " + firstName + " and its dataType is "+ typeof(firstName));
+
+let companyName = 'KRE'; 
+console.log("company name is " + companyName +  " and its dataType is "+  typeof(companyName));
+
+let mobileNumber = 9790614958;
+console.log("mobile Number is " + mobileNumber + " and its dataType is "+  typeof(mobileNumber));
+
+let isAutomation = true;
+console.log("is Automation is " + isAutomation +  " and its dataType is "+ typeof(isAutomation));
+
+let hasPlaywright ;
+console.log("has Playwright is " + hasPlaywright + " and its dataType is "+  typeof(hasPlaywright));
