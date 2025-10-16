a31155f4b7215060daddeda6ae0a33ff2e53dabd
Padhmasri1103 <srigopu11@gmail.com>
2025-10-16 12:38:50 +0530

    Add files via upload
    Day 2 ( conditional statements and declarations)

---
+++ conditionalStatement.js
@@ -0,0 +1,10 @@
+let mark = 50;
+if(mark>70){
+    console.log("Pass:Mark is greater than 70");
+}else if(mark >55){
+    console.log("Pass:Mark is greater then 55");
+}else if(mark > 45){
+    console.log("Pass:mark is greater then 45");
+}else{
+    console.log("Failed");
+}
---
+++ declaration.js
@@ -0,0 +1,18 @@
+
+var empDetails = 'ASD123';
+empDetails = 12;
+ console.log(empDetails);
+
+ var empDetails = true;
+ console.log(empDetails);
+
+ let studentInfo = 2454;
+ console.log(studentInfo);
+ studentInfo = null;
+console.log(studentInfo);
+
+const piValue = 3.14;
+console.log(piValue);
+piValue = 3;
+console.log(piValue);
+
